e6bf81d3a6b4f6d03ef501fa68fc362c217aaebe
jaswanth <jaswanthgeddanti39@gmail.com>
2024-10-24 11:57:59 +0530

    intial commit

--- RTC_driver.h
+++ RTC_driver.h
@@ -10,3 +10,20 @@
 #define RTCDR_D 0x010 /*Date Data register for RTC 32bit*/
 #define RTCMR_T 0x014 /*Time match register for RTC 32 bit*/
 #define RTCMR_D 0x018 /*Data match register for RTC 32bit*/ 
+
+typedef struct RTC_DATE
+{
+    uint8_t day_of_month;
+    uint8_t day_of_week;
+    uint8_t day_of_year;
+    uint8_t month;
+    uint8_t year;
+}RTC_Date;
+
+typedef struct RTC_Time
+{
+    uint8_t am_pm;
+    uint8_t hours;
+    uint8_t seconds;
+    uint8_t minutes;
+}
